3f8dcb645269e57d8f16057d9bf5780e393b059f
EmnaZayaniEsprit <emna.zayani@esprit.tn>
2014-11-08 02:16:07 +0100

    ajout productServices pour tester controller

---
+++ ECommerce.Service/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Data.Infrastructure;
+using ECommerce.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service
+{
+    class ProductService
+    {
+        static DatabaseFactory dbfactory = new DatabaseFactory();
+        IUnitOfWork utow = new UnitOfWork(dbfactory);
+        public List<product> getProducts()
+        {
+            return utow.ProductRepository.GetAll().ToList();
+        }
+    }
+}
